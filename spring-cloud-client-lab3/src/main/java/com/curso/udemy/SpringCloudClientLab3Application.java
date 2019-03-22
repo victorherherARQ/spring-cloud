7d587ca9639eac5eb7dec5be88c4a9200e44e86f
Victor Hernández Hernández <victor.hernandez@diagroup.com>
2019-03-22 12:33:16 +0100

    Subida laboratorio config server udemy

---
+++ spring-cloud-client-lab3/src/main/java/com/curso/udemy/SpringCloudClientLab3Application.java
@@ -0,0 +1,13 @@
+package com.curso.udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudClientLab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudClientLab3Application.class, args);
+	}
+
+}
